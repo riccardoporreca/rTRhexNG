00f20c90a3fa75d97bdda1842112c3345c076400
Riccardo Porreca <riccardo.porreca@mirai-solutions.com>
2018-04-25 16:09:14 +0200

    Mesch reviewed color, define all colors as a list

--- colors.R
+++ colors.R
@@ -10,3 +10,25 @@ split_green <- "#6666AA"
 jumpbox_blue <- "#228822"
 splitbox_green <- "#222288"
 
+base <- list(
+  bg = magrittr_bg,
+  txt = mirai_dark,
+  full_fill = full_gray,
+  full_stroke = mirai_light,
+  jump_fill = jump_blue,
+  jump_stroke = jumpbox_blue,
+  split_fill = split_green,
+  split_stroke = splitbox_green
+)
+
+mesch <- list(
+  bg = "#f8e29c",
+  txt = "#4d4732",
+  full_fill = "#777b7d",
+  full_stroke = "#3a3b3d",
+  jump_fill = "#6fcc8d",
+  jump_stroke = "#2c9840",
+  split_fill = "#5973c1",
+  split_stroke = "#2c4aa5"
+)
+
